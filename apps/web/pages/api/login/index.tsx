aac1cf1a6a5c9e22eb23be4a25b312919c91b737
shalluv <leon555thai@gmail.com>
2023-01-13 07:26:06 +0700

    ✨ feat: firbase token fetcher

---
+++ apps/web/pages/api/login/index.tsx
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse,
+) {
+	const ticket = req.query;
+	const requestHeaders: any = { ticket };
+
+	const firebaseCustomToken = await fetch('http://localhost:2000/auth/', {
+		headers: requestHeaders,
+		method: 'GET',
+	});
+}
